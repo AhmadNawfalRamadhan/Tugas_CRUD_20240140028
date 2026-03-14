f3ef659a7af6c56058155ca8b89bd382a4bebbfc
AhmadNawfalRamadhan <Ahmadnawfalramadhan@gmail.com>
2026-03-14 11:03:23 +0700

    Nambah Java class KtpAddRequest.java di dto dan codingannya

---
+++ src/main/java/com/deploy/Praktikum2/model/dto/KtpAddRequest.java
@@ -0,0 +1,15 @@
+package com.deploy.Praktikum2.model.dto;
+
+import lombok.Data;
+import java.time.LocalDate;
+
+@Data
+
+public class KtpAddRequest {
+    private Integer Id;
+    private String nomorKtp;
+    private String namaLengkap;
+    private String alamat;
+    private LocalDate tanggalLahir;
+    private String jenisKelamin;
+}
