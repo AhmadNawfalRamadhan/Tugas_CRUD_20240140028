233c618e5d172480a224a4754c02958307f516f4
AhmadNawfalRamadhan <Ahmadnawfalramadhan@gmail.com>
2026-03-14 11:33:53 +0700

    Nambah KtpService.java di Service dan codingannya

--- src/main/java/com/deploy/Praktikum2/service/KtpService.java
+++ src/main/java/com/deploy/Praktikum2/service/KtpService.java
@@ -1,4 +1,14 @@
 package com.deploy.Praktikum2.service;
 
+import com.deploy.Praktikum2.model.dto.KtpAddRequest;
+import com.deploy.Praktikum2.model.dto.KtpDto;
+
+import java.util.List;
+
 public interface KtpService {
+    KtpDto AddKtp(KtpAddRequest request);
+    List<KtpDto> getAllKtp();
+    KtpDto getKtpByID(Integer id);
+    KtpDto UpdateKtp(Integer id, KtpAddRequest request);
+    void DeleteKtp(Integer id);
 }
